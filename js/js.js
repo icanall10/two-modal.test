d7d372ce42f3e2d4215765fcde568e0410a00b03
Marat Darbishev <marat.icanall@ya.ru>
2021-04-07 15:30:46 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,42 @@
+(function ($) {
+
+    $(document).ready(function () {
+
+
+        $('.modal')
+            .on('openModal', function () {
+                $(this).addClass('open');
+            })
+            .on('closeModal', function () {
+                $(this).removeClass('open');
+            });
+
+
+        $('[data-modal-link]')
+            .click(function () {
+                let selector = $(this).attr('data-modal-link');
+
+                $(selector).trigger('openModal');
+
+                return false;
+            });
+
+
+        $('.modal .close')
+            .click(function () {
+                let modal = $(this).closest('.modal');
+
+                modal.trigger('closeModal');
+
+                let next = modal.attr('data-model-next') ?? '';
+
+                if (next.length > 0) {
+                    $(next).trigger('openModal');
+                }
+
+                return false;
+            });
+
+    });
+
+})(jQuery);
